79c778b746edb29d069606723a1beee4e28b25fe
Siddheshdalvi07 <dalvisiddhesh3@gmail.com>
2024-02-26 02:22:34 +0530

    portfolio project

--- project portfolio final/main.js
+++ project portfolio final/main.js
@@ -14,24 +14,45 @@ let currentWordIndex = 0;
 let maxWordIndex = words.length - 1;
 words[currentWordIndex].style.opacity = "1";
 
-let changeText = ()=>{
+let changeText = () => {
     let currentWord = words[currentWordIndex];
     let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
 
-    Array.from(currentWord.children).forEach( (letter, i) => {
-       setTimeout(() => {
-        letter.className = "letter out";
-       },i*80);
-      });
-      nextWord.style.opacity="1";
-      Array.from(nextWord.children).forEach((letter,i)=>{
+    Array.from(currentWord.children).forEach((letter, i) => {
+        setTimeout(() => {
+            letter.className = "letter out";
+        }, i * 80);
+    });
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter, i) => {
         letter.className = "letter behind";
-        setTimeout(()=>{
-            letter.className ="letter in";
-        },340+i*80);
-      });
-      currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+        setTimeout(() => {
+            letter.className = "letter in";
+        }, 340 + i * 80);
+    });
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
 };
 
 changeText();
-setInterval(changeText,3000)
+setInterval(changeText, 3000)
+
+/* circle skill ///////////////////////////////////// */
+
+const circles = document.querySelectorAll(".circle");
+circles.forEach(elem => {
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points ="";
+    var rotate = 360 / dots;
+
+    for(let i = 0 ; i<dots;i++){
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for(let i=0; i<percent; i++){
+        pointsMarked[i].classList.add('marked');
+    }
+})

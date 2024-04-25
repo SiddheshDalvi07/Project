3e57a9b95fbdea00b9dd2a9798791b00d48d0c91
Siddheshdalvi07 <dalvisiddhesh3@gmail.com>
2024-04-25 14:32:33 +0530

    django ecom

--- django ecom/Django project/ecom/ecomapp/models.py
+++ django ecom/Django project/ecom/ecomapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    CATEGORY_CHOICES=[(1,'Mobiles'),(2,'Clothes'),(3,'Shoes')]
+    name=models.CharField(max_length=100)
+    price=models.FloatField()
+    pdetails=models.CharField(max_length=300)
+    category=models.IntegerField(choices=CATEGORY_CHOICES)
+    is_active=models.BooleanField(default=True)
+    pimage=models.ImageField(upload_to='images')
+
+
--- django ecom/Django project/ecom/ecomapp/urls.py
+++ django ecom/Django project/ecom/ecomapp/urls.py
@@ -1,2 +1,11 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
+
+urlspattern = [
+        path('',(views.index)),
+]
+
+if settings.DEBUG:
+    urlspattern += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
